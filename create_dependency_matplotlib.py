8eecf28c1d20aa9f4962f84582b1f4de9882dcc3
tadosuke <tadosuke@gmail.com>
2023-09-16 16:41:12 +0900

    matplot で描画するバージョンを追加

---
+++ create_dependency_matplotlib.py
@@ -0,0 +1,81 @@
+"""Python コードを読みこみ、メソッドとフィールドとの関係を matplotlib で描画するツール."""
+
+import argparse
+
+import matplotlib.pyplot as plt
+import networkx as nx
+
+import create_dependency
+
+
+class GraphBuilder:
+    """グラフのノードとエッジを追加するクラス"""
+
+    def __init__(self):
+        """コンストラクタ"""
+        self.G = nx.DiGraph()
+
+    def add_nodes_and_edges(self, class_to_func_data: dict[str, dict[str, list[str]]]) -> None:
+        """ノードとエッジを追加するメソッド"""
+        for class_name, func_to_attr in class_to_func_data.items():
+            self.G.add_node(class_name, color='red')
+
+            for func_name, attrs in func_to_attr.items():
+                full_func_name = f"{class_name}.{func_name}"
+                self.G.add_node(full_func_name, color='blue')
+                self.G.add_edge(class_name, full_func_name)
+
+                for attr in attrs:
+                    full_attr_name = f"{class_name}.{attr}"
+                    self.G.add_node(full_attr_name, color='green')
+                    self.G.add_edge(full_func_name, full_attr_name)
+
+
+class GraphStyler:
+    """グラフにスタイルを適用するクラス"""
+
+    def __init__(self, G: nx.DiGraph):
+        """コンストラクタ"""
+        self.G = G
+
+    def apply_style(self) -> dict:
+        """スタイルを適用するメソッド"""
+        return {node: data['color'] for node, data in self.G.nodes(data=True)}
+
+
+class GraphRenderer:
+    """グラフを描画するクラス"""
+
+    def __init__(self, G: nx.DiGraph):
+        """コンストラクタ"""
+        self.G = G
+
+    def render(self, colors: dict) -> None:
+        """グラフを描画するメソッド"""
+        pos = nx.spring_layout(self.G)
+        nx.draw(self.G, pos, with_labels=True, node_color=list(colors.values()))
+        plt.show()
+
+
+def draw_class_to_func_graph(class_to_func_data: dict[str, dict[str, list[str]]]) -> None:
+    """主要な処理を行う関数"""
+    builder = GraphBuilder()
+    builder.add_nodes_and_edges(class_to_func_data)
+
+    styler = GraphStyler(builder.G)
+    colors = styler.apply_style()
+
+    renderer = GraphRenderer(builder.G)
+    renderer.render(colors)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description='Analyze Python code and draw graph for function and member variable relationships.')
+    parser.add_argument('file_path', type=str, help='Path to the Python file to analyze')
+
+    args = parser.parse_args()
+    file_path = args.file_path
+
+    class_relations = create_dependency.CodeAnalyzer.analyze_code(file_path)
+    draw_class_to_func_graph(class_relations)
