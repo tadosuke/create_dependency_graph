fff8071157c2a0e5410090b39178061ee42c8014
tadosuke <tadosuke@gmail.com>
2023-09-17 06:05:19 +0900

    リファクタリング：クラス分割

--- create_dependency_plantuml.py
+++ create_dependency_plantuml.py
@@ -13,60 +13,58 @@ import create_dependency
 
 
 class PlantUMLGenerator:
-    """PlantUML形式のコードを生成するクラス。"""
-
     @classmethod
     def generate(cls, class_relations: create_dependency.ClassToFuncType) -> str:
         plantuml_code = "@startuml\n"
-
         for class_name, (relations, unused_vars) in class_relations.items():
             plantuml_code += f"rectangle {class_name} {{\n"
-
-            attributes = set()
-            for _, attribute_list in relations.items():
-                attributes.update(attribute_list)
-
-            # 未使用の変数も含める
-            attributes.update(unused_vars)
-
-            plantuml_code += cls._generate_attribute_section(attributes)
-            plantuml_code += cls._generate_function_section(relations)
-            plantuml_code += cls._generate_relations_section(relations)
-            plantuml_code += cls._generate_unused_vars_section(unused_vars)
-
+            plantuml_code += _AttributeSectionGenerator.generate(relations, unused_vars)
+            plantuml_code += _FunctionSectionGenerator.generate(relations)
+            plantuml_code += _RelationsSectionGenerator.generate(relations)
+            plantuml_code += _UnusedVarsSectionGenerator.generate(unused_vars)
             plantuml_code += "}\n"
-
         plantuml_code += "@enduml\n"
         return plantuml_code
 
+
+class _AttributeSectionGenerator:
     @classmethod
-    def _generate_attribute_section(cls, attributes: set[str]) -> str:
+    def generate(cls, relations: dict[str, list[str]], unused_vars: set[str]) -> str:
+        attributes = set()
+        for _, attribute_list in relations.items():
+            attributes.update(attribute_list)
+        attributes.update(unused_vars)
         attribute_section = ""
         for attribute in attributes:
             attribute_section += f"() {attribute}\n"
         return attribute_section
 
+
+class _FunctionSectionGenerator:
     @classmethod
-    def _generate_function_section(cls, relations: dict[str, list[str]]) -> str:
+    def generate(cls, relations: dict[str, list[str]]) -> str:
         function_section = ""
         for function_name, _ in relations.items():
             function_section += f"() f_{function_name}\n"
         return function_section
 
+
+class _RelationsSectionGenerator:
     @classmethod
-    def _generate_relations_section(cls, relations: dict[str, list[str]]) -> str:
+    def generate(cls, relations: dict[str, list[str]]) -> str:
         relations_section = ""
         for function_name, attribute_list in relations.items():
             for attribute in attribute_list:
                 relations_section += f"f_{function_name} --> {attribute}\n"
         return relations_section
 
+
+class _UnusedVarsSectionGenerator:
     @classmethod
-    def _generate_unused_vars_section(cls, unused_vars: set[str]) -> str:
-        """未使用変数のPlantUMLセクションを生成する"""
+    def generate(cls, unused_vars: set[str]) -> str:
         unused_vars_section = ""
         for unused_var in unused_vars:
-            unused_vars_section += f"() {unused_var} <<unused>>\n"  # <<unused>> ステレオタイプを付与
+            unused_vars_section += f"() {unused_var} <<unused>>\n"
         return unused_vars_section
 
 
