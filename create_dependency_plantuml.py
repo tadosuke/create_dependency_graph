69daa29a62ee323ba6bcaff80d5473b437c1df07
tadosuke <tadosuke@gmail.com>
2023-09-16 15:41:52 +0900

    コードをリファクタリング

--- create_dependency_plantuml.py
+++ create_dependency_plantuml.py
@@ -1,6 +1,9 @@
+"""Python コードを読みこみ、メソッドとフィールドとの関係を PlantUML 形式で出力するツール."""
+
+from __future__ import annotations
+
 import ast
 import argparse
-from typing import Dict, List
 
 
 class CodeReader:
@@ -16,11 +19,27 @@ class CodeReader:
 class CodeAnalyzer:
     """Pythonコードを解析するクラス。"""
 
-    @staticmethod
-    def analyze_class_node(class_node: ast.ClassDef) -> Dict[str, List[str]]:
+    @classmethod
+    def analyze_code(cls, code: str) -> dict[str, dict[str, list[str]]]:
+        """Pythonコードを解析して、各クラスの関数とメンバー変数の関係を表す辞書を生成する。"""
+        tree = ast.parse(code)
+        class_relations = {}
+
+        for node in ast.walk(tree):
+            if isinstance(node, ast.ClassDef):
+                class_relations[node.name] = cls._analyze_class_node(node)
+
+        return class_relations
+
+    @classmethod
+    def _analyze_class_node(cls, class_node: ast.ClassDef) -> dict[str, list[str]]:
         """クラスのASTノードを解析して、関数とメンバー変数の関係を表す辞書を生成する。"""
         relations = {}
 
+        # プロパティ一覧を取得
+        properties = CodeAnalyzer._get_properties(class_node)
+
+        # 関数と属性の関係を解析
         for sub_node in ast.walk(class_node):
             if isinstance(sub_node, ast.FunctionDef):
                 function_name = sub_node.name
@@ -29,28 +48,29 @@ class CodeAnalyzer:
                 for attr_node in ast.walk(sub_node):
                     if isinstance(attr_node, ast.Attribute):
                         attribute_name = attr_node.attr
-                        relations[function_name].append(attribute_name)
+
+                        if attribute_name not in properties:
+                            relations[function_name].append(attribute_name)
 
         return relations
 
-    @staticmethod
-    def analyze_code(code: str) -> Dict[str, Dict[str, List[str]]]:
-        """Pythonコードを解析して、各クラスの関数とメンバー変数の関係を表す辞書を生成する。"""
-        tree = ast.parse(code)
-        class_relations = {}
-
-        for node in ast.walk(tree):
-            if isinstance(node, ast.ClassDef):
-                class_relations[node.name] = CodeAnalyzer.analyze_class_node(node)
-
-        return class_relations
+    @classmethod
+    def _get_properties(cls, class_node: ast.ClassDef) -> set[str]:
+        """プロパティ名の一覧を取得する."""
+        properties = set()
+        for sub_node in ast.walk(class_node):
+            if isinstance(sub_node, ast.FunctionDef):
+                for decorator in sub_node.decorator_list:
+                    if isinstance(decorator, ast.Name) and decorator.id == "property":
+                        properties.add(sub_node.name)
+        return properties
 
 
 class PlantUMLGenerator:
     """PlantUML形式のコードを生成するクラス。"""
 
-    @staticmethod
-    def generate(class_relations: Dict[str, Dict[str, List[str]]]) -> str:
+    @classmethod
+    def generate(cls, class_relations: dict[str, dict[str, list[str]]]) -> str:
         """関数とメンバー変数の関係を表す辞書からPlantUML形式のコードを生成する。"""
         plantuml_code = "@startuml\n"
 
@@ -58,21 +78,42 @@ class PlantUMLGenerator:
             plantuml_code += f"class {class_name} {{\n"
 
             attributes = set()
-            for function_name, attribute_list in relations.items():
-                plantuml_code += f"entity func_{function_name}\n"
+            for _, attribute_list in relations.items():
                 attributes.update(attribute_list)
 
-            for attribute in attributes:
-                plantuml_code += f"() {attribute}\n"
-
-            for function_name, attribute_list in relations.items():
-                for attribute in attribute_list:
-                    plantuml_code += f"func_{function_name} --> {attribute}\n"
+            plantuml_code += cls._generate_attribute_section(attributes)
+            plantuml_code += cls._generate_function_section(relations)
+            plantuml_code += cls._generate_relations_section(relations)
 
             plantuml_code += "}\n"
 
         plantuml_code += "@enduml\n"
         return plantuml_code
+
+    @classmethod
+    def _generate_attribute_section(cls, attributes: set[str]) -> str:
+        """属性のPlantUMLセクションを生成する"""
+        attribute_section = ""
+        for attribute in attributes:
+            attribute_section += f"() {attribute}\n"  # circle として表現
+        return attribute_section
+
+    @classmethod
+    def _generate_function_section(cls, relations: dict[str, list[str]]) -> str:
+        """関数のPlantUMLセクションを生成する"""
+        function_section = ""
+        for function_name, _ in relations.items():
+            function_section += f"entity f_{function_name}\n"
+        return function_section
+
+    @classmethod
+    def _generate_relations_section(cls, relations: dict[str, list[str]]) -> str:
+        """関数と属性の関係のPlantUMLセクションを生成する"""
+        relations_section = ""
+        for function_name, attribute_list in relations.items():
+            for attribute in attribute_list:
+                relations_section += f"f_{function_name} --> {attribute}\n"
+        return relations_section
 
 
 if __name__ == "__main__":
